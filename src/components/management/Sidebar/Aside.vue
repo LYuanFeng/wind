2a38bc568826ae954629fed636c63b6f5af5e46b
Lee.Li <Lee.Li@royaleinternational.com>
2019-12-03 18:28:00 +0800

    注释内容

--- src/components/management/Sidebar/Aside.vue
+++ src/components/management/Sidebar/Aside.vue
@@ -4,11 +4,24 @@
       <img src='/static/img/logo.png' class="logo"/>
     </div>
     <div class="nav-sidebar">
-      <div style="height:200px;width: 100px;">4444</div>
-      <div style="height:200px;width: 100px;">4444</div>
-      <div style="height:200px;width: 100px;">4444</div>
-      <div style="height:200px;width: 100px;">4444</div>
-      <div style="height:200px;width: 100px;">4444</div>
+      <el-collapse accordion>
+        <div>站点维护</div>
+        <el-collapse-item>
+          <template slot="title">
+            一致性 Consistency<i class="header-icon el-icon-info"></i>
+          </template>
+          <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+          <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+        </el-collapse-item>
+        <el-collapse-item>
+          <template slot="title">
+            一致性 Consistency<i class="header-icon el-icon-info"></i>
+          </template>
+          <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+          <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+        </el-collapse-item>
+      </el-collapse>
+      
     </div>
   </aside>
 </template>
